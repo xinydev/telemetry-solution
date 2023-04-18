655990dd368a14f78313d33cbd462728c2711ced
Nick Forrington <nick.forrington@arm.com>
2023-04-18 10:22:23 +0000

    topdown_tool: Add N2 metric data
    * Add Neoverse N2 metric data
    * Add tests for Neoverse N2
    * Update test data for Neoverse N2
    
    Change-Id: I5e95cceeae0eee13056c838006fd920c1c19d43b

--- tools/topdown_tool/tests/test_cpu_mapping.py
+++ tools/topdown_tool/tests/test_cpu_mapping.py
@@ -9,6 +9,7 @@ def test_get_cpu():
     assert get_cpu("0x00000000410fd0c0") == "neoverse-n1"
     assert get_cpu("0x00000000411fd0c0") == "neoverse-n1"
     assert get_cpu("0x00000000410fd400") == "neoverse-v1"
+    assert get_cpu("0x00000000410fd490") == "neoverse-n2"
 
 
 def test_no_mapping():
